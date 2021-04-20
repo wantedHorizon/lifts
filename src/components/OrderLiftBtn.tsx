bc8ca011d2ff314fbbd0a788084b86bace0bdf56
unknown <eliran.amzalag@sergata.com>
2021-04-20 18:38:08 +0300

    lifts here

--- src/components/OrderLiftBtn.tsx
+++ src/components/OrderLiftBtn.tsx
@@ -2,16 +2,22 @@ import React from 'react'
 
 interface Props {
     data:any;
-    onClick:()=>void;
+    orderLift:()=>void;
 }
 
 const style = {
     width:'19%',
     // textTransform: ,
 }
-export default function OrderLiftBtn({data,onClick}:Props) {
+export default function OrderLiftBtn({data,orderLift}:Props) {
+    const onClickHandler = ()=>{
+        if(data.status !=='call'){
+            return;
+        }
+        orderLift();
+    }
     return (
-            <button style={style} onClick={onClick}>
+            <button style={style} onClick={onClickHandler}>
                 {data&&data.status.charAt(0).toUpperCase()+data.status.slice(1)}
             </button>
     )
--- src/containers/LiftManager.tsx
+++ src/containers/LiftManager.tsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import FloorCell from "../components/FloorCell";
 import Lift from "../components/Lift/Lift";
 import OrderLiftBtn from "../components/OrderLiftBtn";
-import classes from './LiftManager.module.css';
+import classes from "./LiftManager.module.css";
 const createCellsMatrix = (): any[][] => {
   const mat = [];
   for (let floorLevel = 0; floorLevel < 10; floorLevel++) {
@@ -23,17 +23,57 @@ interface State {
   floors: any[];
   calls: number[];
   mat: any[][];
-  currentlyMoving:number;
+  currentlyMoving: number;
 }
 interface Props {}
 export default class LiftManager extends Component<Props, State> {
   state = {
     lifts: [
-      { id: 0, floor: 0,from:0,to:0, called: false,transitionTime:1,color:'' },
-      { id: 1, floor: 0,from:0,to:0, called: false,transitionTime:1,color:''},
-      { id: 2, floor: 0,from:0,to:0, called: false,transitionTime:1,color:'' },
-      { id: 3, floor: 0,from:0,to:0, called: false,transitionTime:1,color:'' },
-      { id: 4, floor: 0,from:0,to:0, called: false,transitionTime:1,color:'' },
+      {
+        id: 0,
+        floor: 0,
+        from: 0,
+        to: 0,
+        called: false,
+        transitionTime: 1,
+        color: "",
+      },
+      {
+        id: 1,
+        floor: 0,
+        from: 0,
+        to: 0,
+        called: false,
+        transitionTime: 1,
+        color: "",
+      },
+      {
+        id: 2,
+        floor: 0,
+        from: 0,
+        to: 0,
+        called: false,
+        transitionTime: 1,
+        color: "",
+      },
+      {
+        id: 3,
+        floor: 0,
+        from: 0,
+        to: 0,
+        called: false,
+        transitionTime: 1,
+        color: "",
+      },
+      {
+        id: 4,
+        floor: 0,
+        from: 0,
+        to: 0,
+        called: false,
+        transitionTime: 1,
+        color: "",
+      },
     ],
     floors: [
       { level: 9, status: "call" },
@@ -47,71 +87,134 @@ export default class LiftManager extends Component<Props, State> {
       { level: 1, status: "call" },
       { level: 0, status: "call" },
     ],
-    calls:  Array<number>(),
-    currentlyMoving:0,
+    calls: Array<number>(),
+    currentlyMoving: 0,
     mat: createCellsMatrix(),
   };
-
-  componentDidMount(){
+  componentDidUpdate(prevProps:any,prevState:State){
+    if(prevState.currentlyMoving===5 && this.state.currentlyMoving===4 && this.state.calls.length>0){
+      const calls=this.state.calls.slice(1);
+      this.setState({calls});
+      this.orderLift(this.state.calls[0])
+      console.log(prevState);
+      
+    }
+  }
+  componentDidMount() {
     // setTimeout(()=> {
     //   const lifts =this.state.lifts.slice();
-    //   lifts[4]= {...lifts[4],floor:5,from:0,to:5,transitionTime:5,color:'green',called:true} 
+    //   lifts[4]= {...lifts[4],floor:5,from:0,to:5,transitionTime:5,color:'green',called:true}
     //   this.setState({
     //     lifts
     //   })
     // },2000)
   }
 
-  startChange = (to:number,from:number,liftId:number,) => {
-    if(to===from) return ;
+  freeLiftAndFloor = (liftId: number, floorLevel: number) => {
+    const lifts = this.state.lifts.slice();
+    lifts[liftId] = {
+      ...lifts[liftId],
+      color: "",
+      called: false,
+      from: floorLevel,
+      to: floorLevel,
+      floor: floorLevel,
+    };
 
-    this.setState((prev)=>{
-      return {currentlyMoving:prev.currentlyMoving+1}
+    const btns = this.state.floors.slice();
+    btns[floorLevel].status = "call";
+    this.setState((prev) => {
+      return { lifts, floors: btns, currentlyMoving: prev.currentlyMoving - 1 };
+    });
+  };
+
+  changeLiftToArrived = (liftId: number, floorLevel: number) => {
+    const lifts = this.state.lifts.slice();
+    lifts[liftId].color = "green";
+
+    const btns = this.state.floors.slice();
+    btns[floorLevel].status = "arrived";
+    this.setState({ lifts, floors: btns });
+    setTimeout(() => {
+      this.freeLiftAndFloor(liftId, floorLevel);
+    }, 2000);
+  };
+
+  startChange = (to: number, from: number, liftId: number) => {
+    if (to === from) return;
+
+    this.setState((prev) => {
+      return { currentlyMoving: prev.currentlyMoving + 1 };
     });
     //lift moving
     const lift = this.state.lifts[liftId];
-    const newLift = {...lift,from,to,floor:to,transitionTime:Math.abs(to-from),called:true,color:'red'}
+    const newLift = {
+      ...lift,
+      from,
+      to,
+      floor: to,
+      transitionTime: Math.abs(to - from),
+      called: true,
+      color: "red",
+    };
     const liftsArr = this.state.lifts.slice();
-    liftsArr[newLift.id]=newLift;
+    liftsArr[newLift.id] = newLift;
     //button block
     const btns = this.state.floors.slice();
-    const newBtn={...btns[to],status:'waiting'}
-    btns[to]=newBtn;
-    this.setState({lifts:liftsArr,floors:btns});
+    const newBtn = { ...btns[to], status: "waiting" };
+    btns[to] = newBtn;
+    this.setState({ lifts: liftsArr });
+    setTimeout(() => {
+      this.changeLiftToArrived(liftId, to);
+    }, Math.abs(to - from + 1) * 1000);
+  };
 
-  }
+  orderLift = (floor: number) => {
+    console.log(floor, "floor");
+     //button block
+     const btns = this.state.floors.slice();
+     const newBtn = { ...btns[floor], status: "waiting" };
+     btns[floor] = newBtn;
+     this.setState({  floors: btns });
 
-  orderLift = (floor:number) => {
-    console.log(floor,"floor");
-    
-    const availableLifts = this.state.lifts.filter(l=> !l.called);
-    if(availableLifts.length===0){
+    const availableLifts = this.state.lifts.filter((l) => !l.called);
+    if (availableLifts.length === 0) {
       this.state.calls.push(floor);
-    }else if (availableLifts.length===1){
-      this.startChange(floor,availableLifts[0].floor,availableLifts[0].id)
-    }else {
-      const closestLifts = availableLifts.sort(l => Math.abs(floor-l.from)); 
-      if(closestLifts[0].floor===floor){
+    } else if (availableLifts.length === 1) {
+      this.startChange(floor, availableLifts[0].floor, availableLifts[0].id);
+    } else {
+      const closestLifts = availableLifts.sort((l) => Math.abs(floor - l.from));
+      if (closestLifts[0].floor === floor) {
         console.log("lift already here");
-        return ;
+        return;
       }
-      this.startChange(floor,closestLifts[0].from,closestLifts[0].id)
+      this.startChange(floor, closestLifts[0].from, closestLifts[0].id);
     }
-  }
+  };
 
   render() {
     return (
       <div className={classes.board} style={{}}>
-        {this.state.mat.map((row,floorLevel) => (
-          <div className={classes.floor} key={floorLevel} style={{backgroundColor:'red',height:`${10}%`}}>
+        {this.state.mat.map((row, floorLevel) => (
+          <div
+            className={classes.floor}
+            key={floorLevel}
+            style={{ backgroundColor: "red", height: `${10}%` }}
+          >
             {row.map((cell) => (
-              <FloorCell {...cell} key={`${cell.floorLevel}-${cell.fromTheLeft}`}/>
+              <FloorCell
+                {...cell}
+                key={`${cell.floorLevel}-${cell.fromTheLeft}`}
+              />
             ))}
-            <OrderLiftBtn data={this.state.floors[9-floorLevel]} onClick={()=>this.orderLift(9-floorLevel)}/>
+            <OrderLiftBtn
+              data={this.state.floors[9 - floorLevel]}
+              orderLift={() => this.orderLift(9 - floorLevel)}
+            />
           </div>
         ))}
-        {this.state.lifts.map(lift=> (
-            <Lift key={lift.id} {...lift}  />
+        {this.state.lifts.map((lift) => (
+          <Lift key={lift.id} {...lift} />
         ))}
       </div>
     );
